9b2af8d91a6c065374a79aa7676c21923d662302
Prushti <52461905+Prushti@users.noreply.github.com>
2019-11-05 16:29:42 -0800

    Update song-list.js

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -1,29 +1,24 @@
 {
   album.songs.forEach( (song, index) => {
-   song.element = $(`
+    song.element = $(`
       <tr>
-        <td>index</td>
-        <td>song.title</td>
-        <td>song.duration</td>
-        <td>${index}</td>
-        <td>${index + 1}</td>
         <td>
           <button>
-            <span class="song-number">${index + 1}</span>
+            <span class="song-number">${index +1}</span>
             <span class="ion-play"></span>
             <span class="ion-pause"></span>
           </button>
         </td>
         <td>${song.title}</td>
-        <td>${song.duration}</td>
-       </tr>
+        <td>${player.prettyTime(song.duration)}</td>
+      </tr>
     `);
-  song.element.on('click', event => {
+
+    song.element.on('click', event => {
       helper.playPauseAndUpdate(song);
-    $('button#play-pause').attr('playState', player.playState);
+      $('button#play-pause').attr('playState', player.playState);
     });
 
-  $('#song-list').append(song.element);   
-});
-
+    $('#song-list').append(song.element);
+  });
 }
